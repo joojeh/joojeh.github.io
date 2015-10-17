ca7ce6cc840da0b3d43edabbcbe3714cbffe0c86
nina <guityirani@Guitys-MacBook-Air.local>
2015-10-17 15:19:22 -0700

    added functions with JQuery and Javascript

--- resources/javascript.js
+++ resources/javascript.js
@@ -1,1 +1,19 @@
-// Your Javascript will go here!
+// Your Javascript will go here
+$(document).ready(function() {
+  /* Add code from the next steps here */
+  $("#copyright").click(function(){ 
+  	$(this).css('color','purple'); 
+
+  	});
+
+  });
+
+   function replaceName() {
+      var myNewName = window.prompt('Pick a new name!');
+      document.getElementById('user-name').innerHTML = myNewName;
+    }
+/*
+    document.onclick = replaceName;
+
+
+  	/*this function changes the color of the copyright you click on to purple */
